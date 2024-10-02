23bc1ecaf14f8c789b8fb8bd99bcbdbddd800555
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2024-10-02 01:21:31 -0400

    week_15 answers

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 require("express-async-errors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+//to access env variables
 require("dotenv").config();
+//routes
 const secretWordRouter = require("./routes/secretWord");
+const jobRouter = require("./routes/jobs");
+//middleware
 const auth = require("./middleware/auth");
-
-const app = express();
-app.use(require("body-parser").urlencoded({ extended: true }));
+//to manage user sessions
 const session = require("express-session");
 
+//express
+const app = express();
+//sets view engine to EJS
+app.set("view engine", "ejs");
+//parses incoming requests
+app.use(require("body-parser").urlencoded({ extended: true }));
+
+//records session in mongo database
 const MongoDBStore = require("connect-mongodb-session")(session);
 const url = process.env.MONGO_URI;
 
@@ -20,6 +33,7 @@ store.on("error", function (error) {
   console.log(error);
 });
 
+//setting configration for user session
 const sessionParms = {
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -28,24 +42,55 @@ const sessionParms = {
   cookie: { secure: false, sameSite: "strict" },
 };
 
+//if running in production enhance security
 if (app.get("env") === "production") {
   app.set("trust proxy", 1); // trust first proxy
   sessionParms.cookie.secure = true; // serve secure cookies
 }
 
+//Enable session management
 app.use(session(sessionParms));
 
-app.set("view engine", "ejs");
-
+//enables flash messages
+app.use(require("connect-flash")());
+//import passport
 const passport = require("passport");
+//import passport initialization info
 const passportInit = require("./passport/passportInit");
-
+//executes
 passportInit();
+//initialize passport for middleware for handling authentication
 app.use(passport.initialize());
+//persists login session
 app.use(passport.session());
 
-app.use(require("connect-flash")());
+//handling security attacks with csrf protection
+const csrf = require("host-csrf");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(express.urlencoded({ extended: false }));
+let csrf_development_mode = true;
+if (app.get("env") === "production") {
+  csrf_development_mode = false;
+  app.set("trust proxy", 1);
+}
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(helmet());
+app.use(xss());
+const csrf_options = {
+  protected_operations: ["PATCH"],
+  protected_content_types: ["application/json"],
+  development_mode: csrf_development_mode,
+};
+
+app.use(csrf(csrf_options));
 app.use(require("./middleware/storeLocals"));
 app.get("/", (req, res) => {
   res.render("index");
@@ -55,6 +100,8 @@ app.use("/sessions", require("./routes/sessionRoutes"));
 app.use("/secretWord", secretWordRouter);
 
 app.use("/secretWord", auth, secretWordRouter);
+
+app.use("/jobs", auth, jobRouter);
 
 //handles any routes that don't exist
 app.use((req, res) => {
